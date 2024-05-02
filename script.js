d80a2d65123acb37bf8a073af8e691909b599f12
Aaron <aakiswa@gmail.com>
2024-05-02 23:49:21 +0800

    Add sum function

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const add = (n1, n2) => {
-      return num1 + num2;
+      return n1 + n2;
 }
 const subtract = (n1, n2) => {
-      return num1 - num2;
+      return n1 - n2;
 }
 const multiply = (n1, n2) => {
-      return num1 * num2;
+      return n1 * n2;
 }
 const divide = (n1, n2) => {
-      return num1/num2;
+      return n1/n2;
 }
 
 let num1;
@@ -18,18 +18,41 @@ let operator;
 const operate = (n1, n2, operator) => {
       switch(operator) {
             case '+':
-                  add(n1, n2);
-                  break;
+                  return add(n1, n2);
             case '-':
-                  subtract()
+                  return subtract(n1, n2);
+            case '*':
+                  return multiply(n1, n2);
+            case '/':
+                  return divide(n1, n2);
       }
 }
 
+
+
+let display = [];
+
+const view = document.querySelector(".calc-result h1");
 const btns = Array.from(document.querySelectorAll("button"));
 
 btns.forEach((btn) => {
       btn.addEventListener('click', function(event) {
             let btnText = event.target.textContent;
-            console.log(btnText);
+            display.push(btnText);
+            if(btnText === 'C'){
+                  display = [];
+            }else if(btnText === '='){
+                  display.pop();
+                  if(display.includes('+')){
+                        num1 = Number(display.slice(0, display.indexOf('+')).join(''));
+                        num2 = Number(display.slice(display.indexOf('+')+1, display.length).join(''));
+                        operator = '+';
+                        let total = operate(num1, num2, operator);
+                        view.textContent = total;
+                        
+                  }
+            }else{
+                  view.textContent = display.join('');
+            }
       });
 })
